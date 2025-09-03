d12d5e27cfcd9ae62e8a247948fae53e6641ecf7
ann-johansson <ann.johansson.work@gmail.com>
2025-09-03 14:27:29 +0200

    Second change from branch 1.

--- Program.cs
+++ Program.cs
@@ -5,6 +5,8 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            Console.WriteLine("From my first Branch.")
+            Console.WriteLine("My second change in this Branche.");
         }
     }
 }
